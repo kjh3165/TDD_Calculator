6355787db6732143f365be2fc5cb9b8e88dfd38d
kjh3165 <jhwk109@gmail.com>
2026-04-20 17:15:23 +0900

    t8~t9

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -3,17 +3,21 @@ package com.ll;
 public class Calc {
     public static int run(String expression) {
         String[] expressionBits = expression.split(" \\+ ");
+        int result = 0;
 
-        if(expressionBits.length == 1){
-            expressionBits = expression.split(" - ");
-            int num1 = Integer.parseInt(expressionBits[0]);
-            int num2 = Integer.parseInt(expressionBits[1]);
-            return num1 - num2;
+        for(int i=0; i<expressionBits.length; i++){
+            String[] innerBits = expressionBits[i].split(" - ");
+            if(innerBits.length == 1) continue;
+            int num1 = Integer.parseInt(innerBits[0]);
+            int num2 = Integer.parseInt(innerBits[1]);
+            expressionBits[i] =  String.valueOf(num1 - num2);
         }
-        int num1 = Integer.parseInt(expressionBits[0]);
-        int num2 = Integer.parseInt(expressionBits[1]);
 
-        return num1 + num2;
+        for(String bit : expressionBits){
+            int bitToNum = Integer.parseInt(bit);
+            result += bitToNum;
+        }
+        return result;
     }
 
 }
